cbb8c69e3c66ebcf7c332a7e063586d343e5bf6c
SkanderThabet <skander.thabet@esprit.tn>
2022-08-20 03:53:20 +0100

    hero repo injected

--- src/main/kotlin/com/example/routes/AllHeroes.kt
+++ src/main/kotlin/com/example/routes/AllHeroes.kt
@@ -1,17 +1,24 @@
 package com.example.routes
 
 import com.example.models.ApiResponse
+import com.example.repository.HeroRepository
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import org.koin.ktor.ext.inject
 
 fun Route.getAllHeroes() {
+    val heroRepository: HeroRepository by inject()
     get("/boruto/heroes") {
         try {
             val page = call.request.queryParameters["page"]?.toInt() ?: 1
             require(page in 1..5) { "Heroes not found" }
-            call.respond(page)
+            val apiResponse = heroRepository.getAllHeroes(page)
+            call.respond(
+                message = apiResponse,
+                status = HttpStatusCode.OK
+            )
         } catch (e: NumberFormatException) {
             call.respond(
                 message = ApiResponse(
